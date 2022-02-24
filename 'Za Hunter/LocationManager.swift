f3a3e8409be3439c7ab41ebd04ecf3cd1f930e37
Victor <victor>
2022-02-24 10:44:01 -0600

    Added user location (somewhat working)

---
+++ 'Za Hunter/LocationManager.swift
@@ -0,0 +1,21 @@
+//
+//  LocationManager.swift
+//  'Za Hunter
+//
+//  Created by Student on 2/24/22.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+}
+
